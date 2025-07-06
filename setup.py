43b576a3d1ac9439265c8fa724e941ab5ac5471a
Naeema Alnaqbi <naeemaa298@gmail.com>
2025-07-06 18:16:05 +0400

    Fix CLI entry point for GitHub Actions

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='file-encryptor',
     version='1.0.0',
-    description='Simple file encryption tool using Fernet',
-    author='Naeema Alnaqbi',
     packages=find_packages(),
     install_requires=[
-        'cryptography',
+        'cryptography'
     ],
     entry_points={
         'console_scripts': [
-            'file-encryptor=encryptor.file_encryptor_improved:main',
+            'file-encryptor = encryptor.file_encryptor_improved:main',
         ],
     },
+    author='Your Name',
+    description='A simple file encryption/decryption CLI tool',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
 )
